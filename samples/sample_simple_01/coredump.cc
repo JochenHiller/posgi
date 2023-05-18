6d3e74f699b9425cd64926c123f1369036fb4090
Jochen Hiller <jo.hiller@gmail.com>
2023-05-18 22:19:51 +0200

    Added more samples for testing

---
+++ samples/sample_simple_01/coredump.cc
@@ -0,0 +1,21 @@
+/**
+ * @brief Simple program to check core dumps.
+ */
+
+#include <iostream>
+
+int main() {
+  std::cout << "Create core dump!\n";
+
+  std::string s = "Hello, world!";
+  auto p = &s;
+  std::cout << *p << std::endl;
+
+  // TODO: Trace/BPT trap: 5 what does this mean?
+  // on macOS: see /Library/Logs/DiagnosticReports
+  // ls -alt /Library/Logs/DiagnosticReports/ | head
+  p = nullptr;
+  std::cout << *p << std::endl;
+
+  return 0;
+}
